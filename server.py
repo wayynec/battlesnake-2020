675f40ab67e93393dc547c714f7efd3b09d6a59c
Kenichi Obata <kenobata@MacPro.local>
2020-04-22 18:31:01 -0700

    changed outlook

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ class Battlesnake(object):
         # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
         print("START")
-        return {"color": "#888888", "headType": "regular", "tailType": "regular"}
+        return {"color": "#399AF2", "headType": "silly", "tailType": "curied"}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
