67951e3ab3f84cc51a24959aee93407d999bb046
Kenichi Obata <kenobata@MacPro.local>
2020-04-23 21:20:57 -0700

    get enemy's size

--- server.py
+++ server.py
@@ -250,10 +250,25 @@ class Battlesnake(object):
     #---------------------------------------------------------------------
     #Function Priority moves: Retruns priority movement
     def priority(matrix, head, possible_moves):
-        priority_moves=["up", "down", "left", "right"]
+        priority_moves = ["up", "down", "left", "right"]
         # check if the distance between my head and enemy's head (=4) is =< 2
-
-    
+        
+        #enemy is at the right within range 2. head[0] is width
+        if head[0]+2 <= width-1 and matrix[head[0]+2][head[1]] == 4:
+            priority_moves.remove("right")
+        
+        if head[0]-2 >= 0 and matrix[head[0]-2][head[1]] == 4:
+            priority_moves.remove("left")
+        
+        if head[1]+2 <= height-1 and matrix[head[0]][head[1]+2] == 4:
+            priority_moves.remove("down")
+        
+        if head[1]+2 >= 0 and matrix[head[0]][head[1]-2] == 4:
+            priority_moves.remove("up")
+        
+        # match possible moves and priority moves.
+        combined_move = list( set(possible_moves) & set(priority_moves) )
+        move =random.choice(combined_move)
     
         return move
     
